e49337767b3d2f42dce31d56c03e16286a1f4486
Willy <willy548798@gmail.com>
2025-02-11 16:50:43 +0800

    update searchbar v1

---
+++ MyUIkit2025/NewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewCell.swift
+//  MyUIkit2025
+//
+//  Created by Willy Hsu on 2025/2/10.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
